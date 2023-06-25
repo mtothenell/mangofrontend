33f293401cad259edcab832bcebfa2143826a1d5
mnell <mtothenell@gmail.com>
2023-06-25 12:47:43 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -4,40 +4,46 @@ import {useEffect, useState} from "react";
 
 function App() {
 
-  async function save() {
+    async function save() {
 
-    const getTimeList = await fetch("http://localhost:8080/api/getTimeList")
-    const timeList = await getTimeList.json();
+        const getTimeList = await fetch("http://localhost:8080/api/getTimeList", {
+            method: 'GET',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            }
+        })
+        const timeList = await getTimeList.json();
+        setTime(timeList[1].time)
+    }
 
-    setTime(timeList[1].time)
-  }
+    const [time, setTime] = useState();
 
-  const [time, setTime] = useState();
+    useEffect(() => {
+        save().then(r => {
+        })
+    }, [])
 
-  useEffect( () => {
-    save().then(r => {})
-  },[])
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mango Padel
-        </p>
-        <p>
-          Members: {time}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Mango Padel
+                </p>
+                <p>
+                    Members: {time}
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
