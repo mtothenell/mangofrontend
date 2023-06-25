316f444f5d38e17f32616315eac7d1b2e2226094
mnell <mtothenell@gmail.com>
2023-06-25 09:08:37 +0200

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  async function save() {
+
+    const getTimeList = await fetch("http://localhost:8080/api/getTimeList")
+    const timeList = await getTimeList.json();
+
+    setTime(timeList[1].time)
+  }
+
+  const [time, setTime] = useState();
+
+  useEffect( () => {
+    save().then(r => {})
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +25,9 @@ function App() {
         <p>
           Mango Padel
         </p>
-
+        <p>
+          Members: {time}
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
